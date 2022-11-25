1986ee7147afa072c1990596eaa3c5b3e1a85899
OgabekDev <ogabekdev@gmail.com>
2022-11-25 14:04:26 +0500

    Initial Commit

---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Ogabek Matyakubov on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
