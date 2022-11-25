5527d8cdb0786320c5cdbfbbfcb1773e9d0d03bd
OgabekDev <ogabekdev@gmail.com>
2022-11-25 14:06:53 +0500

    All Done

---
+++ MyDemo/screens/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  MyDemo
+//
+//  Created by Ogabek Matyakubov on 25/11/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text("OgabekDev")
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
